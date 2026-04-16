b5ba51ab3830186132d2bd894a4c08d2fa16a156
Siarhei Surdo <esser.nik.ge@gmail.com>
2026-04-16 21:01:14 +0300

    Initialize Spring Boot backend

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO quote (phrase) VALUES ('Success is not final, failure is not fatal: it is the courage to continue that counts.') ON CONFLICT DO NOTHING;
+INSERT INTO quote (phrase) VALUES ('To be or not to be, that is the question.') ON CONFLICT DO NOTHING;
+INSERT INTO quote (phrase) VALUES ('The only limit to our realization of tomorrow will be our doubts of today.') ON CONFLICT DO NOTHING;
+INSERT INTO quote (phrase) VALUES ('Do what you can, with what you have, where you are.') ON CONFLICT DO NOTHING;
+INSERT INTO quote (phrase) VALUES ('It always seems impossible until it''s done.') ON CONFLICT DO NOTHING;
